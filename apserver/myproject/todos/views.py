8e6ea3cd2b002663d45b8b237fa3520390e85c9d
sugiyama404 <mmingriha@gmail.com>
2025-04-17 20:30:34 +0900

    Implement Todo API with CRUD operations and health check endpoint

--- apserver/myproject/todos/views.py
+++ apserver/myproject/todos/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Todo
+from .serializers import TodoSerializer
+from django.utils import timezone
+
+@api_view(['GET'])
+def health_check(request):
+    """
+    ヘルスチェックエンドポイント
+    """
+    return Response({"status": "healthy"}, status=status.HTTP_200_OK)
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    TodoのCRUD操作を行うためのViewSet
+    """
+    queryset = Todo.objects.filter(deleted_at__isnull=True)
+    serializer_class = TodoSerializer
+
+    def perform_destroy(self, instance):
+        # 論理削除を行う
+        instance.deleted_at = timezone.now()
+        instance.save()
